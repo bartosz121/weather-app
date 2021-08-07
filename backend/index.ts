6179dfdea05078e3d1ac58a72d093881f2a1065c
bartosz121 <bmagiera121@gmail.com>
2021-08-07 14:11:58 +0200

    Remove redundant if statement and fixed response status

--- backend/index.ts
+++ backend/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import * as dotenv from 'dotenv';
 
 import Logger from './logger';
+import fetchForecast from './utils';
 
 dotenv.config();
 
@@ -11,21 +12,23 @@ if (!process.env.PORT) {
   process.exit(1);
 }
 
-const port = parseInt(process.env.PORT, 10); // make sure parse int does not recieve undefined
+const port = parseInt(process.env.PORT, 10);
 
 app.get('/api/forecast', (req: Request, res: Response) => {
   Logger.debug(`***${req.method} request from ${req.hostname}(${req.ip})***`);
   const lat: number = Number(req.query.lat);
   const lon: number = Number(req.query.lon);
-  if (!(lat && lon)) {
-    Logger.warn('/api/forecast called without params');
-    res.status(400).json({
-      message: 'Latitude and longitude not found',
+  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely&units=metric&appid=${process.env.API_KEY}`;
+  Logger.debug(`***Fetching forecast for (${lat}, ${lon})***`);
+  fetchForecast(url)
+    .then((data) => res.status(data.status).json(data))
+    .catch((error) => {
+      Logger.debug(error);
+      res.status(error.status).json({
+        status: error.status,
+        error_msg: error.statusText,
+      });
     });
-  } else {
-    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely&units=metric&appid=${process.env.API_KEY}`;
-    res.send(`lat: ${lat} lon: ${lon} url: ${url}`);
-  }
 });
 
 app.listen(port, () => {
