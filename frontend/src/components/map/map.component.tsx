559fa63e65a331e09a78195f405ae3ae41b68d6e
bartosz121 <bmagiera121@gmail.com>
2021-08-10 17:28:44 +0200

    Add Map component.
    - Added leaflet map with geosearch control.
    - AddMarker function which creates map marker onclick and when user clicks on geosearch result.
    - Add styles.
    - Fixed bug with AddMarker (by creating GeoSearchStopOnClick) where it would create a marked when geosearch controll was clicked.

--- frontend/src/components/map/map.component.tsx
+++ frontend/src/components/map/map.component.tsx
@@ -1,33 +1,78 @@
-import React, { useState, useEffect } from 'react';
-import { Map as LeafletMap, LatLngExpression as LatLng } from 'leaflet';
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+import { useState } from 'react';
+import { LatLngExpression as LatLng, LeafletMouseEvent } from 'leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvent } from 'react-leaflet';
+//@ts-ignore
+import { SearchControl, OpenStreetMapProvider } from 'react-leaflet-geosearch';
 
 import LeafletContainer from './map.styles';
 
-const placeMarker = (map: LeafletMap, position: LatLng): void => {
-    map.flyTo(position);
+const GeoSearchStopOnClick = () => {
+  const map = useMap()
+  map.getContainer()
+  //@ts-ignore
+  map.getContainer().lastChild.onclick = e => { e.stopPropagation() }
+  return null;
 }
 
-const Map = () => {
-    const [position, setPosition] = useState<LatLng>([20, 0])
+const AddMarker = () => {
+  const [position, setPosition] = useState<LatLng | null>(null);
+  const flyToMaxZoomLevel = 10;
+  const map = useMap();
 
-    return (
-        <LeafletContainer>
-            <MapContainer
-                center={position}
-                zoom={3}
-                scrollWheelZoom={false}>
-                <TileLayer
-                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-                <Marker position={position}>
-                    <Popup>
-                        {position.toString}
-                    </Popup>
-                </Marker>
-            </MapContainer>
-        </LeafletContainer>
-    )
+  useMapEvent('click', (e: LeafletMouseEvent) => {
+    map.flyTo(e.latlng, map.getZoom() > flyToMaxZoomLevel ? map.getZoom() : flyToMaxZoomLevel)
+    setPosition(e.latlng)
+  })
+
+  map.addEventListener('geosearch/showlocation', (e) => {
+
+  })
+
+  // TODO https://github.com/smeijer/leaflet-geosearch#results
+  map.on('geosearch/showlocation', (e: any) => {
+    const pos: LatLng = [
+      parseFloat(e.location.y),
+      parseFloat(e.location.x),
+    ]
+    setPosition(pos)
+  })
+
+  return position === null ? null : (
+    <Marker position={position}></Marker>
+  );
+};
+
+const Map = () => {
+  const provider = OpenStreetMapProvider();
+
+
+  return (
+    <LeafletContainer>
+      <MapContainer
+        center={[20, 0]}
+        zoom={3}
+        minZoom={3}
+        scrollWheelZoom={true}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <SearchControl
+          provider={provider}
+          style={'button'}
+          position={'bottomright'}
+          showMarker={false}
+          showPopup={false}
+          retainZoomLevel={false}
+          animateZoom={true}
+          autoClose={true}
+          searchLabel={'Enter address'}
+          keepResult={false}
+        />
+        <GeoSearchStopOnClick />
+        <AddMarker />
+      </MapContainer>
+    </LeafletContainer>
+  )
 }
 
 export default Map;
