21a72d71f72afa1066ecf74ef834836c36a5a4f4
bartosz121 <bmagiera121@gmail.com>
2021-08-11 18:52:30 +0200

    Add params validation, if it fails redirect to '/'

--- frontend/src/pages/ForecastPage/ForecastPage.component.tsx
+++ frontend/src/pages/ForecastPage/ForecastPage.component.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import { useParams } from 'react-router';
+import { RouteComponentProps, withRouter, Redirect } from 'react-router-dom';
+
+import { validateParams } from './ForecastPage.utils';
 
 interface params {
     lat: string,
     lng: string
 }
 
-const ForecastPage = () => {
+const ForecastPage = ({ history }: RouteComponentProps) => {
     const { lat, lng } = useParams<params>();
+    if (!validateParams(lat, lng)){
+        history.push('/')
+    }
+
+    
     return (
         <div>
             <p>lat: {lat}</p>
@@ -16,4 +24,4 @@ const ForecastPage = () => {
     )
 }
 
-export default ForecastPage
+export default withRouter(ForecastPage)
