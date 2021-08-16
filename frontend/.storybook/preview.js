deeca2078e22ab37de0e8cabb960fee9891472cf
bartosz121 <bmagiera121@gmail.com>
2021-08-16 19:02:30 +0200

    Add viewports addon and custom ones

--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -1,4 +1,43 @@
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+
+const customViewports = {
+  smallDevice: {
+    name: 'Small Device 576px',
+    styles: {
+      width: '576px',
+      height: '800px',
+    },
+  },
+  mediumDevice: {
+    name: 'Medium Device 768px',
+    styles: {
+      width: '768px',
+      height: '800px',
+    },
+  },
+  largeDevice: {
+    name: 'Large Device 992px',
+    styles: {
+      width: '992px',
+      height: '800px',
+    },
+  },
+  extraLargeDevice: {
+    name: 'Extra Large Device 1200px',
+    styles: {
+      width: '1200px',
+      height: '800px',
+    },
+  },
+}
+
 export const parameters = {
+  viewport: {
+    viewports: {
+      ...INITIAL_VIEWPORTS,
+      ...customViewports,
+    }
+  },
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
