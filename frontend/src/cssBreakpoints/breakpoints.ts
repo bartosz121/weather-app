327719bd1b6d0b5e24137c6a24dfe5b0f021b122
bartosz121 <bmagiera121@gmail.com>
2021-08-16 19:01:04 +0200

    Add css breakpoints to import in component styles

---
+++ frontend/src/cssBreakpoints/breakpoints.ts
@@ -0,0 +1,13 @@
+const size = {
+    small: '576px',
+    medium: '768px',
+    large: '992px',
+    extraLarge: '1200px'
+}
+
+export const device = {
+    small: `(min-width: ${size.small})`,
+    medium: `(min-width: ${size.medium})`,
+    large: `(min-width: ${size.large})`,
+    extraLarge: `(min-width: ${size.extraLarge})`,
+}
