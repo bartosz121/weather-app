9886888626d906750e6e88aeeeb423a412f2f559
bartosz121 <bmagiera121@gmail.com>
2021-08-17 13:23:36 +0200

    Add background for storybook testing

---
+++ frontend/src/stories/background-for-testing.styles.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const TestingBackground = styled.div`
+    background-color: #abe9cd;
+    background-image: linear-gradient(315deg, #abe9cd 0%, #3eadcf 74%);
+`
+
+export default TestingBackground;
