f21bf167b0e8baa432aaffc846bb05d5e121faa5
bartosz121 <bmagiera121@gmail.com>
2021-08-17 13:23:57 +0200

    Add Forecast-header story

---
+++ frontend/src/stories/Forecast-header.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import ForecastHeader from '../components/forecast-header/forecast-header.component';
+
+import { forecastExample, locationExample } from './example.data';
+import TestingBackground from './background-for-testing.styles';
+
+import './font.css'
+
+export default {
+    title: 'Forecast Page Components/Forecast Header',
+    component: ForecastHeader,
+} as ComponentMeta<typeof ForecastHeader>;
+
+const Template: ComponentStory<typeof ForecastHeader> = (args) => <TestingBackground><ForecastHeader {...args} /></TestingBackground>
+
+export const Header = Template.bind({});
+Header.args = {
+    current: forecastExample.forecastData.current,
+    locationName: locationExample,
+}
