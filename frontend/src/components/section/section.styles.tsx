af0033e8af47232d216b7efaebbc28d84c26f5fa
bartosz121 <bmagiera121@gmail.com>
2021-08-18 15:50:00 +0200

    Add 'Section' styles
     - Add 'gallery' class style

--- frontend/src/components/section/section.styles.tsx
+++ frontend/src/components/section/section.styles.tsx
@@ -2,11 +2,75 @@ import styled from 'styled-components'
 
 const SectionContainer = styled.div`
     color: white;
-    padding: 15px 0;
+    padding: 15px 20px;
+
+    .gallery {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: nowrap;
+        overflow-x: auto;
+        -webkit-user-select: none;  /* Chrome all / Safari all */
+        -moz-user-select: none;     /* Firefox all */
+        -ms-user-select: none;      /* IE 10+ */
+        user-select: none;          /* Likely future */   
+        -webkit-overflow-scrolling: touch;
+
+        scrollbar-color: rgba(255, 255, 255, 0.6) rgba(255, 255, 255, 0.3);
+        scrollbar-width: thin;
+
+        &::-webkit-scrollbar {
+            height: 8px;
+            width: 8px;
+        }
+        
+        &::-webkit-scrollbar-track {
+            background-color: rgba(255, 255, 255, 0.3);
+            border-radius: 100px;
+        }
+        
+        &::-webkit-scrollbar-thumb {
+            background-color: rgba(255, 255, 255, 0.6);
+            border-radius: 100px;
+        }
+
+
+
+        .card {
+            height: fit-content;
+            flex-basis: 150px;
+            flex-grow: 1;
+            flex-shrink: 0;
+
+            &-feels-like, &-pop {
+                color: rgba(255, 255, 255, 0.6);
+                font-size: 0.9em;
+            }
+
+            &-pop {
+                justify-content: center;
+                align-items: center;
+            }
+
+            &-description {
+                text-transform: capitalize;
+                text-align: center;
+                width: 95%;
+                white-space: nowrap;
+                overflow: hidden;
+                text-overflow: ellipsis;
+
+                &:hover{
+                    white-space: normal;
+                    overflow: visible;
+                    text-overflow: unset;
+                }
+            }
+        }
+    }
+    
 
     .section-title {
-        font-size: 1.3em;
-        font-weight: 600;
+        font-size: 1.5em;
     }
 `
 
