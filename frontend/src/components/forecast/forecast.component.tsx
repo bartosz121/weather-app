5ecc8bc992904de79943400fed97f176b56c05df
bartosz121 <bmagiera121@gmail.com>
2021-08-18 15:50:57 +0200

    Add 'Hourly' and 'Daily' section

--- frontend/src/components/forecast/forecast.component.tsx
+++ frontend/src/components/forecast/forecast.component.tsx
@@ -1,9 +1,13 @@
 import { useState, useEffect } from 'react';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
+import { ReactSkycon } from 'react-skycons-extended';
+import { GiWaterDrop } from 'react-icons/gi';
 
-import { getLocationName } from './forecast.utils';
+import { getLocationName, mapIconToSkycon, unixToStringShort2Digit, unixToStringHourMinute } from './forecast.utils';
 import { ForecastContainer } from './forecast.styles';
 import ForecastHeader from '../forecast-header/forecast-header.component';
+import Section from '../section/section.component';
+import { Card } from '../card/card.component';
 
 import { ForecastResponse } from '../../interfaces/forecast/forecastResponse';
 
@@ -55,8 +59,48 @@ const Forecast = ({ lat, lng, history }: ForecastProps) => {
 
     return (
         <ForecastContainer>
-            {loading ? <h1>loading</h1> 
-            : <ForecastHeader locationName={location} current={forecast!.forecastData.current}/>}
+            {loading 
+            ? 
+            <h1>loading</h1> 
+            :
+            <div>
+                <ForecastHeader className='forecast-header' locationName={location} current={forecast!.forecastData.current}/>
+                <Section className='section section-daily' title='Daily' >
+                    <div className="gallery">
+                        {
+                            forecast?.forecastData.daily.map((day, index) => (
+                                <Card key={`day${index}`} className={`card day${index}`}>
+                                    <span className='card-dt'>{unixToStringShort2Digit(day.dt, forecast.forecastData.timezone)}</span>
+                                    <div className='card-icon'>
+                                        <ReactSkycon icon={mapIconToSkycon(day.weather[0].icon)} color='white' size={64}/>
+                                    </div>
+                                    <span className='card-description'>{day.weather[0].description}</span>
+                                    <span className='card-temp'>{day.temp.day}&#176;C</span>
+                                    <span className='card-feels-like'>{day.feels_like.day}&#176;C</span>
+                                </Card>
+                            ))
+                        }
+                    </div>
+                </Section>
+                <Section className='section section-hourly' title='Hourly'>
+                <div className="gallery">
+                        {
+                            forecast?.forecastData.hourly.map((hour, index) => (
+                                <Card key={`hour${index}`} className={`card hour${index}`}>
+                                    <span className='card-dt'>{unixToStringHourMinute(hour.dt, forecast.forecastData.timezone)}</span>
+                                    <div className='card-icon'>
+                                        <ReactSkycon icon={mapIconToSkycon(hour.weather[0].icon)} color='white' size={64}/>
+                                    </div>
+                                    <span className='card-description'>{hour.weather[0].description}</span>
+                                    <span className='card-temp'>{hour.temp}&#176;C</span>
+                                    <span className='card-pop'><GiWaterDrop />{Math.round(hour.pop*100)}%</span>
+                                </Card>
+                            ))
+                        }
+                    </div>
+                </Section>
+            </div>
+            }
         </ForecastContainer>
     )
 }
