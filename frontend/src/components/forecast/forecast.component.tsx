16c592c74eeb6317439f9042438b008ade442191
bartosz121 <bmagiera121@gmail.com>
2021-08-22 16:55:30 +0200

    Add opacity animation to loading screen and forecast

--- frontend/src/components/forecast/forecast.component.tsx
+++ frontend/src/components/forecast/forecast.component.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
+import { useTransition, animated, config } from '@react-spring/web';
 
 import LoadingScreen from '../loading-screen/loading-screen.component';
 import ForecastHeader from '../forecast-header/forecast-header.component';
@@ -16,81 +17,99 @@ import { ForecastResponse } from '../../interfaces/forecast/forecastResponse';
 const EXPRESS_PORT = process.env.REACT_APP_EXPRESS_PORT;
 
 interface ForecastProps extends RouteComponentProps {
-    lat: number,
-    lng: number,
+  lat: number,
+  lng: number,
 }
 
 const Forecast = ({ lat, lng, history }: ForecastProps) => {
 
-    const [loading, setLoading] = useState<boolean>(true);
-    const [forecast, setForecast] = useState<ForecastResponse>();
-    const [location, setLocation] = useState<string>("");
-    const [selectedDay, setSelectedDay] = useState<number>(0);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [forecast, setForecast] = useState<ForecastResponse>();
+  const [location, setLocation] = useState<string>("");
+  const [selectedDay, setSelectedDay] = useState<number>(0);
 
-    useEffect(() => {
-        (async () => {
-            const forecastURL = `http://localhost:${EXPRESS_PORT}/api/forecast?lat=${lat}&lon=${lng}`
-            const locationURL = `https://nominatim.openstreetmap.org/reverse.php?lat=${lat}&lon=${lng}&addressdetails=0&format=jsonv2`
+  const transitions = useTransition(loading, {
+    config: config.slow,
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+    delay: 200
+  });
 
-            // Forecast
-            await fetch(forecastURL)
-                .then(response => response.json())
-                .then(forecastRes => {
-                    setForecast(forecastRes);
-                    console.log(forecastRes);
-                })
-                .catch(error => {
-                    alert(`${error} Redirecting...`);
-                    history.push('/');
-                })
+  useEffect(() => {
+    (async () => {
+      const forecastURL = `http://localhost:${EXPRESS_PORT}/api/forecast?lat=${lat}&lon=${lng}`
+      const locationURL = `https://nominatim.openstreetmap.org/reverse.php?lat=${lat}&lon=${lng}&addressdetails=0&format=jsonv2`
 
-            // Location
-            await fetch(locationURL)
-                .then(response => response.json())
-                .then(locationData => {
-                    setLocation(getLocationName(locationData));
-                    console.log(locationData);
-                })
-                .catch(error => {
-                    alert(`${error} Redirecting...`);
-                    history.push('/');
-                })
+      // Forecast
+      await fetch(forecastURL)
+        .then(response => response.json())
+        .then(forecastRes => {
+          setForecast(forecastRes);
+          console.log(forecastRes);
+        })
+        .catch(error => {
+          alert(`${error} Redirecting...`);
+          history.push('/');
+        })
 
-            setLoading(false);
-        })()
-    }, [])
+      // Location
+      await fetch(locationURL)
+        .then(response => response.json())
+        .then(locationData => {
+          setLocation(getLocationName(locationData));
+          console.log(locationData);
+        })
+        .catch(error => {
+          alert(`${error} Redirecting...`);
+          history.push('/');
+        })
 
-    return (
-        <div>
-            {loading
-                ?
-                <LoadingScreen />
-                :
-                <ForecastContainer>
-                        {/* Header */}
-                        <ForecastHeader className='forecast-header' locationName={location} current={forecast!.forecastData.current} timezone={forecast!.forecastData.timezone} />
+      setLoading(false);
+    })()
+  }, [])
 
-                        {/* Alerts */}
-                        {forecast?.forecastData.alerts ?
-                            <AlertsSection alerts={forecast.forecastData.alerts} timezone={forecast.forecastData.timezone} />
-                            : null}
+  return transitions(({ opacity }, item) =>
+    item ? (
+      <animated.div
+        style={{
+          position: "absolute",
+          opacity: opacity.to({ range: [0.0, 1.0], output: [0, 1] })
+        }}
+      >
+        <LoadingScreen />
+      </animated.div>
+    ) : (
+      <animated.div
+        style={{
+          opacity: opacity.to({ range: [1.0, 0.0], output: [1, 0] })
+        }}
+      >
+          <ForecastContainer>
+          {/* Header */}
+          <ForecastHeader className='forecast-header' locationName={location} current={forecast!.forecastData.current} timezone={forecast!.forecastData.timezone} />
 
-                        {/* Daily */}
-                        <DailySection daily={forecast!.forecastData.daily} timezone={forecast!.forecastData.timezone}
-                            selectedDay={selectedDay} setSelectedDay={setSelectedDay} />
+          {/* Alerts */}
+          {forecast?.forecastData.alerts ?
+              <AlertsSection alerts={forecast.forecastData.alerts} timezone={forecast.forecastData.timezone} />
+              : null}
 
-                        {/* Hourly */}
-                        <HourlySection hourly={forecast!.forecastData.hourly} timezone={forecast!.forecastData.timezone} />
+          {/* Daily */}
+          <DailySection daily={forecast!.forecastData.daily} timezone={forecast!.forecastData.timezone}
+              selectedDay={selectedDay} setSelectedDay={setSelectedDay} />
 
-                        {/* Day Details Section */}
-                        <DayDetailsSection dayDetails={forecast!.forecastData.daily[selectedDay]} timezone={forecast!.forecastData.timezone} />
+          {/* Hourly */}
+          <HourlySection hourly={forecast!.forecastData.hourly} timezone={forecast!.forecastData.timezone} />
 
-                        {/* Footer */}
-                        <Footer />
-                </ForecastContainer>
-            }
-        </div>
+          {/* Day Details Section */}
+          <DayDetailsSection dayDetails={forecast!.forecastData.daily[selectedDay]} timezone={forecast!.forecastData.timezone} />
+
+          {/* Footer */}
+          <Footer />
+  </ForecastContainer>
+      </animated.div>
     )
+  );
 }
 
 export default withRouter(Forecast);
