05ec5694a0bf9ce1664a6165938e7a811310af71
bartosz121 <bmagiera121@gmail.com>
2021-08-23 14:33:06 +0200

    Add selected day date to 'Day Details' section title

--- frontend/src/components/day-details-section/day-details-section.component.tsx
+++ frontend/src/components/day-details-section/day-details-section.component.tsx
@@ -6,6 +6,8 @@ import SunSection from '../sun-section/sun-section.component';
 import MoonSection from '../moon-section/moon-section.component';
 import ProgressbarSection from '../progressbar-section/progressbar-section.component';
 
+import { unixToStringLongWeekDay2Digit } from '../forecast/forecast.utils';
+
 import DayDetailsSectionContainer from './day-details-section.styles';
 import { Daily } from '../../interfaces/forecast/daily';
 
@@ -17,7 +19,7 @@ interface DayDetailsSectionProps {
 const DayDetailsSection = ({ dayDetails, timezone }: DayDetailsSectionProps) => {
     return (
         <DayDetailsSectionContainer>
-            <Section className='section section-day-details' title='Day Details'>
+            <Section className='section section-day-details' title={`Day Details - ${unixToStringLongWeekDay2Digit(dayDetails.dt, timezone)}`}>
                 <div className='day-details-wrapper'>
                     <DayTempSection temp={dayDetails.temp} weatherDesc={dayDetails.weather[0].description} />
                     <SunSection sunData={{sunrise: dayDetails.sunrise, sunset: dayDetails.sunset}} timezone={timezone} />
