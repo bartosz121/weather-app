48f323b26004d1f449bab1aeaf32f7d1314b2e31
bartosz121 <bmagiera121@gmail.com>
2021-08-23 19:14:38 +0200

    Add BackgroundImage styled div
    Wraps ForecastContainer and its background image is set based on weather forecast

--- frontend/src/components/forecast/forecast.styles.tsx
+++ frontend/src/components/forecast/forecast.styles.tsx
@@ -1,5 +1,11 @@
 import styled from "styled-components";
 
+import { getBackgroundImage } from './background';
+
+interface BackgroundImageProps {
+    icon: string,
+}
+
 export const ForecastContainer = styled.div`
     height: 100%;
     width: 100%;
@@ -9,4 +15,13 @@ export const ForecastContainer = styled.div`
         width: 80%;
         margin: 0 auto;
     }
+`
+
+export const BackgroundImage = styled.div<BackgroundImageProps>`
+    background: no-repeat center center fixed; 
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+    background-image: url(${props => getBackgroundImage(props.icon)});
 `
